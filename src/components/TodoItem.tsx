00a11e41a1d950eeb54bca3a6e26948a9e9be38e
anusha-c18 <anushac2001@gmail.com>
2023-10-08 22:02:01 +0530

    feat: animations for todo list items

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,22 @@
 import "./TodoItem.css";
+import { motion } from "framer-motion";
 
 const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className="item">{props.text}</li>;
+  return (
+    <motion.li
+      initial={{ y: -10 }}
+      animate={{ y: 0, scale: 1, opacity: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 200,
+        damping: 9,
+        duration: 0.6,
+      }}
+      className="item"
+    >
+      {props.text}
+    </motion.li>
+  );
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 import "./Todos.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul className="todos">
-      {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
-      ))}
-    </ul>
+    <AnimatePresence>
+      <ul className="todos">
+        {props.items.map((item) => (
+          <TodoItem key={item.id} text={item.text} />
+        ))}
+      </ul>
+    </AnimatePresence>
   );
 };
 
