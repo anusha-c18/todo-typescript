534279ce7dae327e6f22393066a11c628684144b
anusha-c18 <anushac2001@gmail.com>
2023-10-08 20:10:54 +0530

    feat: data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Apples"), new Todo("Oranges")];
+
   return (
     <>
-      <Todos />
+      <Todos items={todos} />
     </>
   );
 }
---
+++ src/models/todo.ts
@@ -0,0 +1,13 @@
+class Todo {
+  //todo object should have an id and text field
+
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.id = new Date().toISOString();
+    this.text = todoText;
+  }
+}
+
+export default Todo;
