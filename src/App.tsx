fe575821546240329d4729ec0bf961aa125fb938
anusha-c18 <anushac2001@gmail.com>
2023-10-09 00:32:37 +0530

    wrap app with recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import Todos from "./components/Todos";
 import NewTodo from "./components/NewTodo";
-import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <RecoilRoot>
+    <>
       <NewTodo />
       <Todos />
-    </RecoilRoot>
+    </>
   );
 }
 
