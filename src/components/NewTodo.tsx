e6230575d34affb79542c92bd5fcf5293b619272
anusha-c18 <anushac2001@gmail.com>
2023-10-08 22:04:36 +0530

    feat: dispatch react hot toast notifactions for input validation

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,5 +1,8 @@
 import { useRef } from "react";
 import "./NewTodo.css";
+import toast, { Toaster } from "react-hot-toast";
+
+const notify = (text: string) => toast(text);
 
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
@@ -9,6 +12,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const enteredText = todoTextInputRef.current!.value;
     //input validation
     if (enteredText.trim().length === 0) {
+      notify("No text entered!");
       return;
     } else {
       props.onAddTodo(enteredText);
@@ -16,11 +20,14 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" ref={todoTextInputRef} />
-      <button>Add Todo</button>
-    </form>
+    <>
+      <Toaster />
+      <form onSubmit={submitHandler}>
+        <label htmlFor="text">Todo text</label>
+        <input type="text" id="text" ref={todoTextInputRef} />
+        <button>Add Todo</button>
+      </form>
+    </>
   );
 };
 
